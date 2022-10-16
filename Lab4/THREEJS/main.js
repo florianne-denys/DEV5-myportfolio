5b57580935ccbf074ef7763a4f48c4a5f2a6cd5e
florianne-denys <r0836751@student.thomasmore.be>
2022-10-16 12:23:09 +0200

    feat: add background

--- Lab4/THREEJS/main.js
+++ Lab4/THREEJS/main.js
@@ -6,6 +6,7 @@ import House from './src/House';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 10;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -17,6 +18,13 @@ const controls = new OrbitControls( camera, renderer.domElement );
 //light 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
+//add light in house
+const addLight = (x, y, z) => {
+	const light = new THREE.PointLight(0xffffff, 0.5);
+	light.position.set(x, y, z);
+	scene.add(light);
+}
+addLight(0, 5, 0);
 
 //add directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
@@ -27,10 +35,15 @@ scene.add(directionalLight);
 const house = new House();
 scene.add(house.group);
 
-
-
-
-camera.position.z = 5;
+//add sphere 
+const sphereGeometry = new THREE.SphereGeometry( 100, 32, 32 );
+const sphereMaterial = new THREE.MeshLambertMaterial( {
+	color: 0x808080,
+	map: new THREE.TextureLoader().load( './public/assets/textures/sky.jpeg' ),
+	side: THREE.BackSide,
+} );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+scene.add( sphere );
 
 function animate() {
   requestAnimationFrame( animate );
